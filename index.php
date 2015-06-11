d75e1f470db81249a48d3e2c01ace4954a189834
Cade Gillem <cpgillem@gmail.com>
2015-06-10 23:49:34 -0400

    Add basic page.

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    require_once "main.php";
+?>
+
+<html>
+    <head>
+        <title><?php echo TITLE; ?></title>
+    </head>
+    <body>
+        <h1>Test</h1>
+    </body>
+</html>
---
+++ main.php
@@ -0,0 +1,8 @@
+<?php
+    /* This is the main PHP configuration file. This should hold constants
+    and utility functions for the entire website to use. */
+
+    define(TITLE, "Tower Plower");
+    define(DEVELOPER, "C-Squared");
+    define(DESCRIPTION, "A tower defense game that'll plow your towers.");
+?>
