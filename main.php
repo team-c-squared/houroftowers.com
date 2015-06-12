ea465358388ec89c4a47456b3881cba724bc4f08
Cade Gillem <cpgillem@gmail.com>
2015-06-11 22:05:34 -0400

    Some documentation.

--- main.php
+++ main.php
@@ -2,7 +2,13 @@
     /* This is the main PHP configuration file. This should hold constants
     and utility functions for the entire website to use. */
 
+    /* Require any PHP files. */
+
+    /* Define constants. */
+
     define(TITLE, "Hour of Towers");
     define(DEVELOPER, "C-Squared");
     define(DESCRIPTION, "A tower defense game that towers above the rest");
+
+    /* Define functions. */
 ?>
