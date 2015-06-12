4ec08e3299635260f2c797722aed72a638cac763
Cade Gillem <cpgillem@gmail.com>
2015-06-11 20:19:47 -0400

    Change title and description.

--- main.php
+++ main.php
@@ -2,7 +2,7 @@
     /* This is the main PHP configuration file. This should hold constants
     and utility functions for the entire website to use. */
 
-    define(TITLE, "Tower Plower");
+    define(TITLE, "Hour of Towers");
     define(DEVELOPER, "C-Squared");
-    define(DESCRIPTION, "A tower defense game that'll plow your towers.");
+    define(DESCRIPTION, "A tower defense game that towers above the rest");
 ?>
