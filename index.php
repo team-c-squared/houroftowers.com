d2335181981627c02f043363ec451bfce6d740e0
Cade Gillem <cpgillem@gmail.com>
2015-06-11 23:54:09 -0400

    Add mockup of page.

--- index.php
+++ index.php
@@ -8,6 +8,24 @@
     </head>
     <body>
         <?php include "includes/header.php"; ?>
+        <div class="container">
+            <!-- Screenshot carousel -->
+            <div class="row row-centered">
+                <div class="col-md-8 col-md-offset-2">
+                    <img src="/images/screenshots/screenshot_1.png" class="img-responsive"/>
+                </div>
+            </div>
+            <!-- Information Row -->
+            <div class="row row-centered">
+                <!-- Game Description -->
+                <div class="col-md-8">
+                    <p>Hour of towers is a game and stuff. Lorem ipsum something something.</p>
+                </div>
+                <div class="col-md-4">
+                    <p>This is where my dev blog would go. IF I HAD ONE</p>
+                </div>
+            </div>
+        </div>
         <?php include "includes/footer.php"; ?>
     </body>
 </html>
