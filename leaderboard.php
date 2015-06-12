db3533aa1fa51082a53657786fb6d0c94d6fe6a8
Cade Gillem <cpgillem@gmail.com>
2015-06-12 12:50:00 -0400

    Add dummy leaderboard page.

---
+++ leaderboard.php
@@ -0,0 +1,39 @@
+<?php require_once "main.php"; ?>
+
+<html>
+    <head>
+        <?php include "includes/head.php"; ?>
+    </head>
+    <body>
+        <?php include "includes/header.php"; ?>
+        <div class="container">
+            <!-- Leaderboard -->
+            <div class="row row-centered">
+                <div class="col-md-12">
+                    <h1>Leaderboard</h1>
+                    <div class="panel panel-default">
+                        <table class="table table-striped">
+                            <tr>
+                                <th>Username</th>
+                                <th>Score</th>
+                            </tr>
+                            <tr>
+                                <td>swaglord</td>
+                                <td>420</td>
+                            </tr>
+                            <tr>
+                                <td>cpg</td>
+                                <td>210</td>
+                            </tr>
+                            <tr>
+                                <td>sknnywhiteman</td>
+                                <td>-9001</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php include "includes/footer.php"; ?>
+    </body>
+</html>
