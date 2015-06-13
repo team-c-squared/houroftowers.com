3dd973c7c1df60d27079ab41e760962b441bc669
Cade Gillem <cpgillem@gmail.com>
2015-06-13 13:23:46 -0400

    Add Raleway font from Google Fonts.

--- includes/head.php
+++ includes/head.php
@@ -1,3 +1,4 @@
 <title><?php echo TITLE; ?></title>
 <link rel="stylesheet" href="/bower_components/bootstrap/dist/css/bootstrap.css"/>
 <link rel="stylesheet" href="/css/custom.css"/>
+<link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
